8bfcaa6c13abc5e1174af58a7a10690fa5f94809
vaniffatiy <mytsykov73@mail.ru>
2024-06-20 13:41:56 +0300

    hospital_7_stable

--- app/run_commands.py
+++ app/run_commands.py
@@ -4,47 +4,47 @@ from . import commands
 
 
 def receive_and_process_command():
-    command_input = input("Введите команду: ").lower()
-    if command_input not in commands_dict["стоп"]:
-        while True:
-            found = False
-            for c in commands_dict:
-                if command_input in commands_dict[c]:
-                    found = True
-                    process_command(command_input)
-                    if command_input in commands_dict["стоп"]:
-                        print("Сеанс завершён.")
-                        return
-                    command_input = input("Введите команду: ").lower()
-            if not found:
-                print(f'Неизвестная команда! Попробуйте ещё раз')
-                command_input = input("Введите команду: ").lower()
-    else:
-        print("Сеанс завершён.")
+    session_active = True
+    while session_active:
+        command_input = input("Введите команду: ").lower()
+        if command_input in commands_dict["стоп"]:
+            session_active = False
+            print("Сеанс завершён.")
+            return
+        found = False
+        for c in commands_dict:
+            if command_input in commands_dict[c]:
+                found = True
+                process_command(command_input)
+                if command_input in commands_dict["стоп"]:
+                    session_active = False
+                    print("Сеанс завершён.")
+                    break
+        if not found:
+            print(f'Неизвестная команда! Попробуйте ещё раз')
 
 
 def process_command(command: str):
-    if command in commands_dict["узнать"]:
-        commands.get_status_by_id(add_id())
-    elif command in commands_dict["повысить"]:
-        commands.upgrade_status(add_id())
-    elif command in commands_dict["понизить"]:
-        commands.downgrade_status(add_id())
-    elif command in commands_dict["выписать"]:
-        commands.discharge_patient(add_id())
-    elif command in commands_dict["рассчитать"]:
+    if command in commands_dict["рассчитать"]:
         commands.calculate_statistics()
+        return
     elif command in commands_dict["стоп"]:
         return
+    id = add_id()
+    if id is not None:
+        if command in commands_dict["узнать"]:
+            commands.get_status_by_id(id)
+        elif command in commands_dict["повысить"]:
+            commands.upgrade_status(id)
+        elif command in commands_dict["понизить"]:
+            commands.downgrade_status(id)
+        elif command in commands_dict["выписать"]:
+            commands.discharge_patient(id)
 
 
-def add_id() -> int:
+def add_id() -> int | None:
     id_input = input("Введите ID: ")
     if is_id_valid(id_input):
         return int(id_input)
     else:
-        receive_and_process_command()
-
-
-
-
+        return None
